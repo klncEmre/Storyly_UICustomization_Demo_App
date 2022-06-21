5375997bc026dc8c168a3d302e5fddcbf4c50ae3
klncEmre <kilince18@itu.edu.tr>
2022-06-21 11:40:09 +0300

    Views and reset button created

--- Storyly_UICustomization_Demo_App/ViewController.swift
+++ Storyly_UICustomization_Demo_App/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
-
+import Storyly
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var defaultView: StorylyView!
+    @IBOutlet weak var customizedView: StorylyView!
+    let STORYLY_INSTANCE_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NfaWQiOjc2MCwiYXBwX2lkIjo0MDUsImluc19pZCI6NDA0fQ.1AkqOy_lsiownTBNhVOUKc91uc9fDcAxfQZtpm3nj40"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //default
+        defaultView.storylyInit = StorylyInit(storylyId: STORYLY_INSTANCE_TOKEN)
+        defaultView.rootViewController = self
+        defaultView.delegate = self
+        
+        //customized
+        customizedView.storylyInit = StorylyInit(storylyId: STORYLY_INSTANCE_TOKEN)
+        customizedView.rootViewController = self
+        customizedView.delegate = self
+       
     }
 
 
 }
 
+extension ViewController: StorylyDelegate{
+    
+}
