720e4b1c22d7d752da75ac1e577eac5f6b09cf3a
klncEmre <kilince18@itu.edu.tr>
2022-06-22 11:09:16 +0300

    visibility input is ready

--- Storyly_UICustomization_Demo_App/ViewController.swift
+++ Storyly_UICustomization_Demo_App/ViewController.swift
@@ -21,13 +21,12 @@ class ViewController: UIViewController {
    
     var storyGroupTextIsVisible = true
     @IBOutlet weak var textVisibleButton: UIButton!
-    
+    @IBOutlet weak var textColorPicker: UIPickerView!
+    let GroupTextColors = ["Red","Black","Orange","Yellow","Red","Black","Orange","Yellow"]
 //    var storyGroupTextColor
 //    var storyGroupTextFont
 //    var storyGroupTextLines
-    
-    
-    
+
     @IBOutlet weak var viewsContainer: UIView!
     
    
@@ -37,13 +36,18 @@ class ViewController: UIViewController {
         defaultView.storylyInit = StorylyInit(storylyId: STORYLY_INSTANCE_TOKEN)
         defaultView.rootViewController = self
         defaultView.delegate = self
-        
         //customized
         customizedView.storylyInit = StorylyInit(storylyId: STORYLY_INSTANCE_TOKEN)
         customizedView.rootViewController = self
         customizedView.delegate = self
-//        setupScrollView()
-//        setUpTextStyling()
+        //Story Group Text Styling
+        textColorPicker.delegate = self
+        textColorPicker.dataSource = self
+        
+        
+        
+        
+        
         
         }
     
@@ -54,16 +58,38 @@ class ViewController: UIViewController {
         if(storyGroupTextIsVisible){
             storyGroupTextIsVisible = false
             textVisibleButton.setImage(UIImage(systemName: "circle",withConfiguration: UIImage.SymbolConfiguration(scale: .large))?.withTintColor(UIColor.black), for: UIControl.State.normal)
+            
         }
         else{
             storyGroupTextIsVisible = true
             textVisibleButton.setImage(UIImage(systemName: "circle.inset.filled",withConfiguration: UIImage.SymbolConfiguration(scale: .large))?.withTintColor(UIColor.black), for: UIControl.State.normal)
         }
+        customizedView.storyGroupTextIsVisible = storyGroupTextIsVisible
+       
+        
     }
     
     
 }
 
 extension ViewController: StorylyDelegate{
+    
+}
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.GroupTextColors.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return GroupTextColors[row]
+        
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+    }
     
 }
