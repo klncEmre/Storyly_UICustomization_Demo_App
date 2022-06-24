2ffc05f830644a9ad3d99ef124bab9a4d62f8562
klncEmre <kilince18@itu.edu.tr>
2022-06-24 19:46:02 +0300

    seen-notSeen border color system implemented (60%)

--- Storyly_UICustomization_Demo_App/ViewController.swift
+++ Storyly_UICustomization_Demo_App/ViewController.swift
@@ -10,7 +10,7 @@ import Storyly
 class ViewController: UIViewController {
     
     
-    
+//    DEFAULT PICKER VIEW VALUES NEED TO BE UPDATED TO DEFAULT OF STORYLY.
     @IBOutlet weak var defaultView: StorylyView!
     @IBOutlet weak var customizedView: StorylyView!
     let STORYLY_INSTANCE_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhY2NfaWQiOjc2MCwiYXBwX2lkIjo0MDUsImluc19pZCI6NDA0fQ.1AkqOy_lsiownTBNhVOUKc91uc9fDcAxfQZtpm3nj40"
@@ -35,15 +35,23 @@ class ViewController: UIViewController {
     var rgbValues = [[0,0,0],[189,61,16],[0,0,0],[0,0,0]]
     var currentPropertyIndex = 0
     @IBOutlet weak var colorShowcase: UIView!
+
     
 //seen - not seen system
     @IBOutlet weak var seenColorStack: UIStackView!
-    @IBOutlet weak var notSeenColorStack: UIStackView!
+    @IBOutlet weak var notSeenColorView: UIView!
     var colorsOfSeenState : [UIColor] = []
     var colorsOfNotSeenSate: [UIColor] = []
     var rgbValuesOfSeenState:[Int] = [0,0,0]
     var rgbValuesOfNotSeenState:[Int] = [0,0,0]
     @IBOutlet weak var viewsContainer: UIView!
+    @IBOutlet weak var notSeenColorsLabel: UILabel!
+    @IBOutlet weak var stateColorPicker: UIPickerView!
+    var coordinateKeeperNotSeen = 20.0
+    let sizeOfStack = 80
+    var availableIndexes = [0,1,2,3]
+    let colorsAndRGB = ["Red": [186,0,0],"Black":[1,0,0], "Yellow":[2,3,0]]
+    var frontItem = "x"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -70,7 +78,8 @@ class ViewController: UIViewController {
         colorShowcase.layer.cornerRadius = colorShowcase.frame.size.width/2
         colorShowcase.clipsToBounds = true
         colorShowcase.backgroundColor = UIColor.black
-        
+        stateColorPicker.delegate = self
+        stateColorPicker.dataSource = self
         
         
     }
@@ -95,34 +104,44 @@ class ViewController: UIViewController {
     }
     
     @IBAction func notSeenAddButton(_ sender: Any) {
-        let colorStack = UIStackView()
-        colorStack.axis = .horizontal
-        colorStack.alignment = .center
+       
+        let myStak = UIView()
+        myStak.translatesAutoresizingMaskIntoConstraints = false
+
+        notSeenColorView.addSubview(myStak)
+        myStak.widthAnchor.constraint(equalToConstant: CGFloat(sizeOfStack)).isActive = true
+        myStak.heightAnchor.constraint(equalToConstant: CGFloat(30)).isActive = true
+        myStak.leftAnchor.constraint(equalTo: notSeenColorsLabel.rightAnchor , constant: CGFloat(Double(sizeOfStack * availableIndexes[0]) * 1.1)).isActive = true
+        myStak.centerYAnchor.constraint(equalTo: notSeenColorsLabel.centerYAnchor).isActive = true
+    
+        let button = UIButton()
+        button.tag = availableIndexes[0]
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
         
-        let label: UILabel = {
+        button.setImage(UIImage.init(systemName: "clear.fill"), for: .normal)
+
+        lazy var myLabel: UILabel = {
             let label = UILabel()
             label.translatesAutoresizingMaskIntoConstraints = false
-            label.numberOfLines = 0
-            label.text = "(" + rgbValuesOfNotSeenState[0].description + rgbValuesOfNotSeenState[1].description + rgbValuesOfNotSeenState[2].description + ")"
+            label.text = "(" + rgbValuesOfNotSeenState[0].description + ", " +  rgbValuesOfNotSeenState[1].description + ", " + rgbValuesOfNotSeenState[2].description + ")"
+            label.font = UIFont.systemFont(ofSize: 12)
             return label
         }()
-        colorStack.addArrangedSubview(label)
-        let button = UIButton()
-        button.setImage(UIImage.init(systemName: "clear.fill"), for: .normal)
-        button.setTitleColor(.systemBlue,
-                             for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.addTarget(self,
-                         action: #selector(buttonAction),
-                         for: .touchUpInside)
-        colorStack.addArrangedSubview(button)
-        colorStack.backgroundColor = UIColor.systemGray.withAlphaComponent(0.8)
-        colorStack.layer.cornerRadius = colorStack.frame.size.width / 2
-        colorStack.distribution = .equalCentering
-        notSeenColorStack.addArrangedSubview(colorStack)
-        notSeenColorStack.spacing = 20
+        myStak.layer.cornerRadius = 8
+        myStak.backgroundColor = UIColor.lightGray.withAlphaComponent(0.4)
+        myStak.addSubview(button)
+        myStak.addSubview(myLabel)
+        button.widthAnchor.constraint(equalToConstant: CGFloat(Double(sizeOfStack) * 0.4)).isActive = true
+        button.heightAnchor.constraint(equalToConstant: CGFloat(20)).isActive = true
+        button.centerYAnchor.constraint(equalTo: myStak.centerYAnchor).isActive = true
+        button.rightAnchor.constraint(equalTo: myStak.rightAnchor,constant: CGFloat(-3)).isActive = true
+        myLabel.translatesAutoresizingMaskIntoConstraints = false
+        myLabel.leftAnchor.constraint(equalTo: myStak.leftAnchor,constant: CGFloat(2)).isActive = true
+        myLabel.centerYAnchor.constraint(equalTo: myStak.centerYAnchor).isActive = true
         
-        
+        availableIndexes.remove(at: 0)
+        customizedView.storyGroupIconBorderColorNotSeen = colorsOfNotSeenSate
         
        
     }
@@ -133,7 +152,8 @@ class ViewController: UIViewController {
     @objc func buttonAction(_ sender: UIButton){
         
         sender.superview?.removeFromSuperview()
-        
+        availableIndexes.insert(sender.tag, at: 0)
+        availableIndexes.sort()
     }
     
     
@@ -153,7 +173,15 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
         else if (pickerView == stylesAndColors){
             return 4
         }
+        else if(pickerView == stateColorPicker){
+            return 4
+        }
+        else if(pickerView == lineNumberPicker){
+            return 1
+        }
         return 1
+        
+    
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
@@ -168,24 +196,31 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
                 return 5
             case stylesAndColors:
                 return properties.count
+            case stateColorPicker:
+                return colorsAndRGB.count
             default:
                 return 1
             }
             
           
         }
+        else if(component != 0 && pickerView ==  stateColorPicker){
+            return 255
+        }
         else if(component == 1 && pickerView == fontPicker) {
             return 12
         }
-        else if(component != 0 && pickerView == stylesAndColors) {
+        else if(component != 0 && (pickerView == stylesAndColors)) {
             return 255
         }
-        
+       
+    
         return 1
     }
+    
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         
-        if(component == 0) { //MEANS I HAVE ONE ELEMENT IN A ROW.
+        if(component == 0) {
             switch pickerView{
                 case fontPicker:
                     let key = Array(groupTextFontDict.keys)
@@ -194,6 +229,10 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
                     return  (row+1).description
                 case stylesAndColors:
                     return properties[row]
+                case stateColorPicker:
+                    let k = Array(colorsAndRGB.keys)
+                    frontItem = k[row]
+                    return k[row]
                 default:
                     return "x"
             }
@@ -205,9 +244,21 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
         else if(pickerView == stylesAndColors){
             return row.description
         }
-           
-        
-       
+        else if(pickerView == stateColorPicker ) {
+            if(component == 3){
+                let v = Array(colorsAndRGB.values)
+                rgbValuesOfNotSeenState = v[1]
+                stateColorPicker.selectRow(1, inComponent: 0, animated: true)
+                for i in 1...3{
+                    stateColorPicker.selectRow(v[1][i-1], inComponent: i, animated: true)
+                }
+                
+
+            }
+            return row.description
+            
+            
+        }
         return "x"
     }
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
@@ -231,6 +282,13 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
                 }
                 let currentValues = rgbValues[currentPropertyIndex]
                 colorShowcase.backgroundColor = UIColor.init(red: CGFloat(currentValues[0])/255, green: CGFloat(currentValues[1])/255, blue: CGFloat(currentValues[2])/255, alpha: 1.0)
+                break
+            case stateColorPicker:
+                let v = Array(colorsAndRGB.values)
+                rgbValuesOfNotSeenState = v[row]
+                for i in 1...3{
+                    stateColorPicker.selectRow(v[row][i-1], inComponent: i, animated: true)
+                }
                 break
             default:
                 break
@@ -276,7 +334,7 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     }
     func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
         let w = pickerView.frame.size.width
-        if(pickerView == stylesAndColors ){
+        if(pickerView == stylesAndColors  || pickerView == stateColorPicker ){
             if(component == 0){
                 return w * 0.4
             }
@@ -290,6 +348,7 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
             case lineNumberPicker:
                 return w
             
+        
         default:
             return w
         }
